59e0d4c9f2f14cc62ec023f6a6d461869f64ee56
44-2-kupa-martin <kupamartinclassroom@gmail.com>
2022-09-13 14:46:21 -0300

    a

--- WindowsApplication1/WindowsApplication1/Form2.vb
+++ WindowsApplication1/WindowsApplication1/Form2.vb
@@ -60,7 +60,73 @@ Public Class Form2
         TextBox1.Text = TextBox1.Text + "/"
     End Sub
 
-    Private Sub reg()
+    Private Function parseSum(ByVal input As String) As Decimal
+        Dim result() As String = Regex.Split(input, "(\+|-)")
+        Dim sumStack As New List(Of String)
+        Dim finalSum As Decimal = 0
+        Dim subtrahendStack As New List(Of String)
+        Dim finalSubtrahend As Decimal = 0
+        '2 is a sum, 1 is a substraction, 0 is number
+        Dim flag As Integer = 2
+        For Each Str As String In result
+            Select Case flag
+                Case 2
+                    sumStack.Add(Str)
+                Case 1
+                    subtrahendStack.Add(Str)
+            End Select
+            If Str = "-" Then
+                flag = 1
+            ElseIf Str = "+" Then
+                flag = 2
+            Else
+                flag = 0
+            End If
+        Next
+        For Each Str As String In sumStack
+            finalSum = finalSum + parseProduct(Str)
+        Next
+        For Each Str As String In subtrahendStack
+            finalSubtrahend = finalSubtrahend + parseProduct(Str)
+        Next
+        Return finalSum - finalSubtrahend
+    End Function
 
+    Private Function parseProduct(ByVal input As String) As Decimal
+        Dim result() As String = Regex.Split(input, "(\*|/)")
+        Dim productStack As New List(Of Decimal)
+        Dim finalProduct As Decimal = 1
+        Dim quotentStack As New List(Of Decimal)
+        Dim finalQuotent As Decimal = 1
+        '2 is a product, 1 is a division, 0 is a number
+        Dim flag As Integer = 2
+        For Each Str As String In result
+            Select Case flag
+                Case 2
+                    productStack.Add(Str)
+                Case 1
+                    quotentStack.Add(Str)
+            End Select
+            If Str = "/" Then
+                flag = 1
+            ElseIf Str = "*" Then
+                flag = 2
+            Else
+                flag = 0
+            End If
+        Next
+
+
+        For Each Num As Decimal In productStack
+            finalProduct = finalProduct * Num
+        Next
+        For Each Num As Decimal In quotentStack
+            finalQuotent = finalQuotent * Num
+        Next
+        Return finalProduct / finalQuotent
+    End Function
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        TextBox1.Text = CStr(parseSum(TextBox1.Text))
     End Sub
 End Class
